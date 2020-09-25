a846aeb1cdc5cfe8fd38d4e017fbcc98cb52cfc0
hi <kangja1996@gmail.com>
2020-09-24 22:37:49 -0400

    adding explanation

---
+++ linked-list.js
@@ -0,0 +1,11 @@
+// linked list class
+// read, add, remove and so on
+
+// what is linked list?
+// it's a linear data structure and elements are linked together using pointers. We have different nodes within linked list. 
+
+// The first node is called the head. Each node has two things: it has the actual data and it has pointers/reference to next element in the linked list. 
+
+// The very last node one has a tail and it references to null. And this is how you know it's end of the list. 
+
+// Everything stems from the head .
